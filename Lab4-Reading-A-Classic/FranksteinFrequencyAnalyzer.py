8324365797b6034a82d70d178ebf2a10d66c249f
Robert-Jasz <59976330+Robert-Jasz@users.noreply.github.com>
2020-02-06 15:13:01 -0500

    Update FranksteinFrequencyAnalyzer.py
    ahaha

--- Lab4-Reading-A-Classic/FranksteinFrequencyAnalyzer.py
+++ Lab4-Reading-A-Classic/FranksteinFrequencyAnalyzer.py
@@ -14,4 +14,39 @@ for line in frankensteinText:
         chapters[i] = chapters[i] + line
 frankensteinText.close()
 del chapters[:29] #removes all the table of contents
-print(chapters[0])
+Victor = []
+Creature = []
+Agatha = []
+Caroline = []
+De_Lacey = []
+Elizabeth = []
+Ernest = []
+Felix = []
+Henry = []
+Justine = []
+William = []
+
+
+for x in chapters:
+    Victor.append(x.count("Victor"))
+    Creature.append(x.count("Creature"))
+    Agatha.append(x.count("Agatha"))
+    Caroline.append(x.count("Caroline"))
+    De_Lacey.append(x.count("De Lacey"))
+    Elizabeth.append(x.count("Elizabeth"))
+    Ernest.append(x.count("Ernest"))
+    Felix.append(x.count("Felix"))
+    Henry.append(x.count("Henry"))
+    Justine.append(x.count("Justine"))
+    William.append(x.count("William"))
+print("Victor: " + str(Victor))
+print("Creature: " + str(Creature))
+print("Agatha: " + str(Agatha))
+print("Caroline: " + str(Caroline))
+print("De_Lacey: " + str(De_Lacey))
+print("Elizabeth: " + str(Elizabeth))
+print("Ernest: " + str(Ernest))
+print("Felix: " + str(Felix))
+print("Henry: " + str(Henry))
+print("Justine: " + str(Justine))
+print("William: " + str(William))
